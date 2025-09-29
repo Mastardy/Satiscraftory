e0d7c9352ce0bcb3325209779aee120f60ea62b7
André Monteiro <andremgmonteiro01@gmail.com>
2025-09-29 11:35:18 +0100

    Base Fabric Project

---
+++ src/client/java/io/github/mastardy/SatiscraftoryClient.java
@@ -0,0 +1,9 @@
+package io.github.mastardy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SatiscraftoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/io/github/mastardy/Satiscraftory.java
@@ -0,0 +1,17 @@
+package io.github.mastardy;
+
+import io.github.mastardy.items.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Satiscraftory implements ModInitializer {
+    public static final String MOD_ID = "satiscraftory";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
